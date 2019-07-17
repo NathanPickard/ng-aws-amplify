edc36a82cad47c6c0db8fefce1a0cf71735f9b0d
Nathan Pickard <nathan.pickard87@gmail.com>
2019-07-16 22:14:11 -0700

    Initial commit for Angular Amplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { AmplifyService } from 'aws-amplify-angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-aws-amplify';
+
+  signedIn: boolean;
+  user: any;
+  greeting: string;
+
+  constructor(private amplifyService: AmplifyService) {
+    this.amplifyService.authStateChange$
+      .subscribe(authState => {
+        this.signedIn = authState.state === 'signedIn';
+        if (!authState.user) {
+          this.user = null;
+        } else {
+          this.user = authState.user;
+          this.greeting = "Hello " + this.user.username;
+        }
+      })
+  }
 }
